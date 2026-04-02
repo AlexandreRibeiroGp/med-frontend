8cfa44b7722db4b539c66d7fef008880053d158f
Alexandre.Ribeiro@ntconsult.com.br <alexandreribeiro@macro.com.ar>
2026-04-02 09:47:43 -0300

    fix: render runtime ice config correctly

--- infra/nginx/40-runtime-config.sh
+++ infra/nginx/40-runtime-config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -eu
 
+: "${ICE_SERVERS_JSON:=[{\"urls\":\"stun:stun.l.google.com:19302\"}]}"
+
+export ICE_SERVERS_JSON
+
 envsubst '${ICE_SERVERS_JSON}' \
   < /opt/medcallon/runtime-config.template.js \
   > /usr/share/nginx/html/runtime-config.js
--- infra/nginx/runtime-config.template.js
+++ infra/nginx/runtime-config.template.js
@@ -1,3 +1,3 @@
 window.__MEDCALLON_RUNTIME__ = {
-  iceServers: ${ICE_SERVERS_JSON:-[{"urls":"stun:stun.l.google.com:19302"}]}
+  iceServers: ${ICE_SERVERS_JSON}
 };
